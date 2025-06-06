3284d8e119e288779166633f5ed884f6a7ba986e
Roman Popyk <romanpopyk@Romans-MacBook-Pro.local>
2025-06-06 21:16:45 +0100

    trying to add API

--- back-end/app.js
+++ back-end/app.js
@@ -1,2 +1,11 @@
 const safeHTML = DOMPurify.sanitize(etyContent);
 document.getElementById("etymology").innerHTML = safeHTML;
+
+var URL = "https://en.wiktionary.org/w/api.php?action=parse&page=apple&format=json&origin=*"
+const data = await fetch(URL).then(response => response.json());
+const htmlString = data.parse.text["*"];
+
+const parser = new DOMParser();
+const doc = parser.parseFromString(htmlString, "text/html");
+
+document.getElementById("IPA").innerHTML = content;
